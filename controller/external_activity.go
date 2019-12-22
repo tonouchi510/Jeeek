010dc5c37331c4dd13c7fa277994d09062546463
tonouchi510 <tonouchi27@gmail.com>
2019-12-23 02:14:44 +0900

    Update external activity batch

--- controller/external_activity.go
+++ controller/external_activity.go
@@ -4,6 +4,7 @@ import (
 	"cloud.google.com/go/firestore"
 	"context"
 	"firebase.google.com/go/auth"
+	"github.com/tonouchi510/Jeeek/domain/repository"
 	"github.com/tonouchi510/Jeeek/service"
 	"log"
 
@@ -23,9 +24,143 @@ func NewExternalActivity(logger *log.Logger, authClient *auth.Client, fsClient *
 	return &externalActivitysrvc{logger, authClient, fsClient}
 }
 
-// 指定したユーザのQiitaの記事投稿を取得する
-func (s *externalActivitysrvc) FetchQiitaArticle(ctx context.Context, p *externalactivity.SessionTokenPayload) (err error) {
-	s.logger.Print("externalActivity.Fetch qiita article")
+// セッションに紐づくユーザの連携済みサービスのアクティビティを取得する
+func (s *externalActivitysrvc) RefreshActivitiesOfExternalServices(ctx context.Context, p *externalactivity.SessionTokenPayload) (err error) {
+	s.logger.Print("externalActivity.Refresh activities of external services")
+
+	userService := service.NewUserService(ctx, s.authClient)
+	externalService := service.NewExternalServiceService(ctx, s.fsClient)
+	activityService := service.NewActivityService(ctx, s.fsClient)
+
+	// ユーザ情報の取得
+	user, err := userService.GetUserTinyByToken(*p.Token)
+	if err != nil {
+		s.logger.Print(err)
+		return
+	}
+
+	// 連携サービスのアカウントIDの取得
+	accounts, err := externalService.ListServiceAccounts(user.UID)
+	if err != nil || accounts == nil {
+		s.logger.Print(err)
+		return
+	}
+
+	for _, a := range accounts {
+		serviceUid := a.ServiceUID
+
+		var extService repository.ExternalActivityRepository
+		if a.ServiceName == "qiita" {
+			extService = service.NewQiitaService()
+		} else {
+			s.logger.Print("")
+			continue
+		}
+
+		i, N := 0, 3
+		for {
+			// サービス毎のアクティビティを取得
+			activities, err := extService.GetRecentActivityByServiceUID(serviceUid, i+N)
+			if err != nil {
+				s.logger.Print(err)
+				return err
+			}
+
+			// 被らないn件をみる
+			if len(activities)-i <= 0 {
+				break
+			} else if len(activities)-i > N {
+				activities = activities[i:i+N]
+			} else {
+				activities = activities[i:]
+			}
+
+			// アクティビティの保存
+			for _, activity := range activities {
+				activity.User = *user
+			}
+			successes, err := activityService.InsertAll(activities)
+			if err != nil {
+				s.logger.Print(err)
+			}
+			if successes != N {
+				break
+			}
+			i += N
+		}
+	}
+
+	return nil
+}
+
+// セッションに紐づくユーザのQiitaの記事投稿を取得する
+func (s *externalActivitysrvc) RefreshQiitaActivity(ctx context.Context, p *externalactivity.SessionTokenPayload) (err error) {
+	s.logger.Print("externalActivity.Refresh qiita activity")
+
+	userService := service.NewUserService(ctx, s.authClient)
+	externalService := service.NewExternalServiceService(ctx, s.fsClient)
+	qiitaService := service.NewQiitaService()
+	activityService := service.NewActivityService(ctx, s.fsClient)
+
+	// ユーザ情報の取得
+	user, err := userService.GetUserTinyByToken(*p.Token)
+	if err != nil {
+		s.logger.Print(err)
+		return
+	}
+
+	// QiitaアカウントIDの取得
+	account, err := externalService.GetQiitaAccount(user.UID)
+	if err != nil {
+		s.logger.Print(err)
+		return
+	}
+	if account == nil {
+		s.logger.Print(err)
+		return
+	}
+	serviceUid := account.ServiceUID
+
+	// Qiita記事の取得
+	i, N := 0, 3
+	for {
+		// サービス毎のアクティビティを取得
+		activities, err := qiitaService.GetRecentActivityByServiceUID(serviceUid, i+N)
+		if err != nil {
+			s.logger.Print(err)
+			return err
+		}
+		s.logger.Print(len(activities))
+
+		// 被らないn件をみる
+		if len(activities)-i <= 0 {
+			break
+		} else if len(activities)-i > N {
+			activities = activities[i:i+N]
+		} else {
+			activities = activities[i:]
+		}
+
+		// アクティビティの保存
+		for _, activity := range activities {
+			activity.User = *user
+		}
+		successes, err := activityService.InsertAll(activities)
+		if err != nil {
+			s.logger.Print(err)
+		}
+		if successes != N {
+			break
+		}
+		i += N
+	}
+
+	return nil
+}
+
+// サービス連携以前のqiita記事投稿を反映させる
+func (s *externalActivitysrvc) PickOutPastActivityOfQiita(ctx context.Context, p *externalactivity.SessionTokenPayload) (err error) {
+	s.logger.Print("externalActivity.Pick out past activity of qiita")
 
 	// ユーザ情報の取得
 	userService := service.NewUserService(ctx, s.authClient)
@@ -37,7 +172,7 @@ func (s *externalActivitysrvc) FetchQiitaArticle(ctx context.Context, p *externa
 
 	// QiitaアカウントIDの取得
 	externalService := service.NewExternalServiceService(ctx, s.fsClient)
-	res, err := externalService.GetQiita(user.UID)
+	res, err := externalService.GetQiitaAccount(user.UID)
 	if err != nil {
 		s.logger.Print(err)
 		return
@@ -50,25 +185,21 @@ func (s *externalActivitysrvc) FetchQiitaArticle(ctx context.Context, p *externa
 
 	// Qiita記事の取得
 	qiitaService := service.NewQiitaService()
-	activities, err := qiitaService.GetArticleByUserId(serviceUid)
+	activities, err := qiitaService.ListActivityByServiceUID(serviceUid)
 	if err != nil {
 		s.logger.Print(err)
 		return
 	}
-	activities.User = *user
 
-	// Activityの追加
+	// アクティビティの保存
 	activityService := service.NewActivityService(ctx, s.fsClient)
-	err = activityService.Insert(*activities)
+	for _, activity := range activities {
+		activity.User = *user
+	}
+	_, err = activityService.InsertAll(activities)
 	if err != nil {
 		s.logger.Print(err)
 	}
 
 	return err
-}
-
-// サービス連携以前のqiita記事投稿を反映させる
-func (s *externalActivitysrvc) PickOutPastActivityOfQiita(ctx context.Context, p *externalactivity.SessionTokenPayload) (err error) {
-	s.logger.Print("externalActivity.Pick out past activity of qiita")
-	return
 }
