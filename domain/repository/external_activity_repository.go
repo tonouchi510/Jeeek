93ea5e6ac5eb28e33f2240537a4d0d9a085541c2
tonouchi510 <tonouchi27@gmail.com>
2020-01-04 23:32:42 +0900

    Add comment out

--- domain/repository/external_activity_repository.go
+++ domain/repository/external_activity_repository.go
@@ -3,6 +3,7 @@ package repository
 import "github.com/tonouchi510/Jeeek/domain"
 
 type ExternalActivityRepository interface {
-	GetRecentActivityByServiceUID(uid string, num int) (res []*domain.Activity, err error)
 	ListActivityByServiceUID(uid string) (res []*domain.Activity, err error)
+	// 多様な外部サービスとの連携に対応するために不便だがこの様なメソッドにしている
+	GetRecentActivityByServiceUID(uid string, num int) (res []*domain.Activity, err error)
 }
