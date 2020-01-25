12964bee9b9f96a44dc4a6f1c2bc30c49540a38b
tonouchi510 <tonouchi27@gmail.com>
2020-01-25 16:05:40 +0900

    実行コマンドの修正

--- script/run-local.sh
+++ script/run-local.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+if [ "$1" = "" ]
+then
+    echo "no argument"
+    exit 1
+fi
+echo "$1"
+
+export ADMIN_PASSWORD="$1"
 export FIREBASE_CREDENTIALS="firebase-service-key-dev.json"
 export FIREBASE_APIKEY="AIzaSyAF9quvSMu9n3LMWBrXw_aO5LYwBzqT4Gw"
 export GOOGLE_APPLICATION_CREDENTIALS="gae-service-key-dev.json"
