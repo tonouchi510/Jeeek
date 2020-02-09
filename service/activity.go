c72ab1dc07626d63537537ec78c9b35e729be063
tonouchi510 <tonouchi27@gmail.com>
2020-02-09 14:37:41 +0900

    service/activityにメソッドを追加

--- service/activity.go
+++ service/activity.go
@@ -3,12 +3,14 @@ package service
 import (
 	"cloud.google.com/go/firestore"
 	"context"
-	"fmt"
 	"github.com/tonouchi510/Jeeek/domain"
 	"github.com/tonouchi510/Jeeek/domain/repository"
 	"github.com/tonouchi510/Jeeek/service/model"
+	"google.golang.org/api/iterator"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"log"
+	"sort"
 	"time"
 )
 
@@ -19,6 +21,51 @@ type activityService struct {
 
 func NewActivityService(ctx context.Context, client *firestore.Client) repository.ActivityRepository {
 	return &activityService{ctx, client}
+}
+
+func (s activityService) List(uid string) (res []*domain.Activity, err error) {
+	iter := s.fsClient.Collection(model.UserCollection).Doc(uid).
+		Collection(model.ActivityCollection).Documents(s.ctx)
+
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+		var m model.Activity
+		err = doc.DataTo(&m)
+		if err != nil {
+			return nil, err
+		}
+
+		res = append(res, &domain.Activity{
+			ID:        doc.Ref.ID,
+			Category:  m.Category,
+			Content:   domain.Content{
+				Subject:  m.Content.Subject,
+				Url:      m.Content.Url,
+				Comment:  m.Content.Comment,
+			},
+			Rank:      m.Rank,
+			Tags:      m.Tags,
+			Favorites: m.Favorites,
+			Gifts:     m.Gifts,
+			UserTiny:  domain.UserTiny{
+				UID:       m.UserTiny.UID,
+				Name:      m.UserTiny.Name,
+				PhotoUrl:  m.UserTiny.PhotoUrl,
+			},
+			UpdatedAt: m.UpdatedAt,
+		})
+	}
+
+	sort.Slice(res, func(i, j int) bool {
+		return res[i].UpdatedAt.UnixNano() > res[j].UpdatedAt.UnixNano()
+	})
+	return
 }
 
 func (s activityService) InsertAll(uid string, activities []*domain.Activity) (success int, err error) {
@@ -71,7 +118,8 @@ func (s activityService) Insert(uid string, activity domain.Activity) (err error
 		return
 	}
 	if snapshot.Exists() {
-		return fmt.Errorf("error: activity id=%s is already exist", activity.ID)
+		log.Printf("error: activity id=%s is already exist", activity.ID)
+		return nil
 	}
 
 	data := &model.Activity{
@@ -83,6 +131,8 @@ func (s activityService) Insert(uid string, activity domain.Activity) (err error
 		},
 		Rank:      activity.Rank,
 		Tags:      activity.Tags,
+		Favorites: []string{},
+		Gifts:     []string{},
 		UserTiny:      model.UserTiny{
 			UID: activity.UserTiny.UID,
 			Name: activity.UserTiny.Name,
@@ -92,6 +142,13 @@ func (s activityService) Insert(uid string, activity domain.Activity) (err error
 	}
 	_, err = s.fsClient.Collection(model.UserCollection).Doc(uid).
 		Collection(model.ActivityCollection).Doc(activity.ID).Set(s.ctx, data)
+
+	return
+}
+
+func (s activityService) Delete(uid string, activityID string) (err error) {
+	_, err = s.fsClient.Collection(model.UserCollection).Doc(uid).
+		Collection(model.ActivityCollection).Doc(activityID).Delete(s.ctx)
 
 	return
 }
